9a721c46be45d5076df27d1c93a12c4ab139e108
statick33 <alexgilbert33@hotmail.com>
2014-02-26 20:24:05 -0500

    added team page

---
+++ lab3/team.php
@@ -0,0 +1,39 @@
+<?php
+	include('header.php'); 
+?>
+	
+	<h3 style="margin-bottom:0px;float:left;">GTI 350 Dota Team</h3>
+	<div id="button" style="float:right !important;margin-top:10px;margin-left:0px;">Join team</div>
+	<div id="button" style="float:right !important;margin-top:10px;margin-left:0px;">Manage team</div>
+
+	<table style="width:1024px;border-spacing:0 20px;">
+		<tr>
+			<td style="vertical-align:top;">Informations :</td><td style="width:300px;vertical-align:top;"> This is the best dota 2 team in north america idmnaisfnaodsfasodfnsdfansodfnasd <br /> apsodksdopf asdfaosdpfk asdfpokasdpofkas</td>
+			<td colspan="3"><div style="width:450px;height:150px;border:1px solid red;vertical-align:center;text-align:center;">Team Image</div></td>
+			<td colspan="2" style="vertical-align:top;text-align:right;"></td>
+		</tr>
+		<tr style="padding-top:20px;"><td style="vertical-align:top;">Player roster :</td><td style="vertical-align:top;"> 
+			<ul id="matchList" style="list-style-type: none; ">
+				<li style="margin-top:0px !important;"><a href="#">Player 1</a></li>
+				<li><a href="#">Player 2</a></li>
+				<li><a href="#">Player 3</a></li>
+				<li><a href="#">Player 4</a></li>
+				<li><a href="#">Player 5</a></li>
+			</ul>
+			</td><td style="vertical-align:top;">Upcoming Matchs</td><td style="vertical-align:top;">
+				<ul id="matchList" style="list-style-type: none; ">
+					<li style="margin-top:0px !important;"><a href="#">Match 6 <br /><a href="#"> Against Team R</a></li>
+					<li><a href="#">Match 7 </a><br /><a href="#"> Against Team T</a></li>
+					<li><a href="#">Match 8 </a><br /><a href="#"> Against Team W</a></li>
+				</ul>
+			</td><td style="vertical-align:top;">Lastest matchs :</td><td> 
+			<ul id="matchList" style="list-style-type: none; ">
+				<li style="margin-top:0px !important;background-color:#6CBB3C;"><a href="#">Match 4<br /> 2-0 <br /> Against Team G</li>
+				<li style="background-color:#DC381F;"><a href="#">Match 3</a><br /><a href="#"> 1-2 </a><br /><a href="#"> Against Team X</a></li>
+				<li style="background-color:#DC381F;"><a href="#">Match 2</a><br /><a href="#"> 0-2 </a><br /><a href="#"> Against Team S</a></li>
+			</ul>
+			
+	</table>
+<?php
+	include('footer.php'); 
+?>
