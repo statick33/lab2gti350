1c5f0399a53f7820a6925658ff82ca0f108f2976
statick33 <alexgilbert33@hotmail.com>
2014-02-27 14:04:06 -0500

    added match page

---
+++ lab3/match.php
@@ -0,0 +1,42 @@
+<?php
+	include('header.php'); 
+?>
+	<style>
+		table
+		{
+			width:1024px;
+			text-align:center;
+		}
+		table .titleRow
+		{
+			font-weight:bold;
+			font-size:22px;
+		}
+		table .teamImage
+		{
+			width:150px;
+			height:150px;
+			border:1px solid red;
+			margin-left:auto;
+			margin-right:auto;
+		}
+		table #button
+		{
+			margin-left:auto;
+			margin-right:auto;
+			float:none;
+		}
+	</style>
+	<table>
+		<tr class='titleRow'><td></td><td> Team 1</td><td>VS</td><td>Team 2</td></tr>
+		<tr><td></td><td> <div class='teamImage'>Team 1 image</div> </td><td></td><td><div class='teamImage'>Team 2 image</div></td><td></td></tr>
+		<tr><td>Current score :</td><td>2-1</td><td></td><td>1-2</td><td></td></tr>
+		<tr><td>Predictions :</td><td>60%</td><td></td><td>40%</td><td></td></tr>
+		<tr><td></td><td><div id="button">Predict</div></td><td></td><td><div id="button">Predict</div></td></tr>
+		<tr><td>Chat :</td><td colspan="3">Captain 1 : When can you play?</td></tr>
+		<tr><td></td><td colspan="3">Captain 2 : Wednesday 8pm.</td></tr>
+		<tr><td></td><td colspan="3"><textarea style="width:100%;height:100%;"></textarea></td><td style="width:100px;"><div style="width:100px;" id="button">Reply</div></td></tr>
+	
+<?php
+	include('footer.php'); 
+?>
