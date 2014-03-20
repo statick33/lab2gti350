f5e6933d4de918386d1ade2c1671dcd19773c293
statick33 <alexgilbert33@hotmail.com>
2014-03-20 13:54:19 -0400

    match

--- lab3/match.php
+++ lab3/match.php
@@ -2,41 +2,65 @@
 	include('header.php'); 
 ?>
 	<style>
-		table
+		#matchTable
 		{
-			width:1024px;
 			text-align:center;
 		}
-		table .titleRow
+		#matchTable .titleRow
 		{
 			font-weight:bold;
 			font-size:22px;
 		}
-		table .teamImage
+		#matchTable .teamImage
 		{
-			width:150px;
-			height:150px;
-			border:1px solid red;
+			width:200px;
+			height:146px;
+			
 			margin-left:auto;
 			margin-right:auto;
 		}
-		table .button
+		#matchTable .smallButton
 		{
 			margin-left:auto;
 			margin-right:auto;
 			float:none;
 		}
+	    #matchTable textarea
+		{
+			width:100%;
+			height:100%;
+			background-color:black;
+			border:2px solid #2e1a1e;
+			color:white;
+		}
+		#matchTable textarea:focus
+		{
+			border: 2px solid white;
+		}
 	</style>
-	<table>
+	<?php
+		include('mainContentBoxHeader.php'); 
+	?>
+	<?php
+		include('ContentBoxHeader.php'); 
+	?>
+	<h3>Match</h3>
+	<table id="matchTable">
 		<tr class='titleRow'><td></td><td> Team 1</td><td>VS</td><td>Team 2</td></tr>
-		<tr><td></td><td> <div class='teamImage'>Team 1 image</div> </td><td></td><td><div class='teamImage'>Team 2 image</div></td><td></td></tr>
+		<tr><td></td><td> <img src="style/images/team.png" class="teamImage" </td><td></td><td><img src="style/images/team2.png" class="teamImage" /></td><td></td></tr>
 		<tr><td>Current score :</td><td>2-1</td><td></td><td>1-2</td><td></td></tr>
 		<tr><td>Predictions :</td><td>60%</td><td></td><td>40%</td><td></td></tr>
-		<tr><td></td><td><div class="button">Predict</div></td><td></td><td><div class="button">Predict</div></td></tr>
+		<tr><td></td><td><div class="smallButton">Predict</div></td><td></td><td><div class="smallButton">Predict</div></td></tr>
 		<tr><td>Chat :</td><td colspan="3">Captain 1 : When can you play?</td></tr>
 		<tr><td></td><td colspan="3">Captain 2 : Wednesday 8pm.</td></tr>
-		<tr><td></td><td colspan="3"><textarea style="width:100%;height:100%;"></textarea></td><td style="width:100px;"><div style="width:100px;" class="button">Reply</div></td></tr>
-	
+		<tr><td></td><td colspan="3"><textarea></textarea></td><td style="width:100px;"><div style="width:100px;" class="smallButton">Reply</div></td></tr>
+	</table>
+	<?php
+		include('ContentBoxFooter.php'); 
+	?>
+	<?php
+		include('mainContentBoxFooter.php'); 
+	?>
 <?php
 	include('footer.php'); 
 ?>
