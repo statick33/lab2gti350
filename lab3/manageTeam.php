3ce65792f146b8e2be106b08214b9b2689bbbe1d
statick33 <alexgilbert33@hotmail.com>
2014-03-20 13:40:03 -0400

    manageTeam

--- lab3/manageTeam.php
+++ lab3/manageTeam.php
@@ -14,6 +14,13 @@
 		{
 			width:350px;
 			height:80px;
+			background-color:black;
+			border:2px solid #2e1a1e;
+			color:white;
+		}
+		table textarea:focus
+		{
+			border: 2px solid white;
 		}
 		table td
 		{
@@ -22,10 +29,8 @@
 		
 		table .teamImage
 		{
-			width:50px;
-			height:50px;
-			border:1px solid red;
-			float:left;
+			width:100px;
+			height:100px;
 		}
 		
 		table #button
@@ -49,38 +54,77 @@
 			vertical-align:middle;
 		}
 		
+		.twocolumn .columnLeft{
+			width:50%;
+			float:left;
+		}
+		.twocolumn .columnRight{
+			width:50%;
+			float:right;
+		}
+		
 	</style>
+
+		<?php
+			include('mainContentBoxHeader.php'); 
+		?>
 		<h3>Manage Team</h3>
-		<table>
-		<tr>
-			<td>Team informations
-				<table style="border:1px solid black; width:500px;height:344px;">
-					<tr><td>Name :</td><td><input type="text" value="GTI 350 Dota Team"/></td></tr>
-					<tr><td>Informations :</td><td><textarea></textarea></td></tr>
-					<tr><td>Picture :</td><td><div class="teamImage">Preview</div><div class="button">Upload</div></tr>
-					<tr><td></td><td><div class="button" style="float:right;">Save</div></td></tr>
-				</table>
-			</td>
-			<td>Player list
-				<table style="border:1px solid black; width:460px;height:344px;">
+		<div class="twocolumn">
+
+			<div class="columnLeft">
+			<?php
+				include('contentBoxHeader.php'); 
+			?>
+				<h3>Team informations</h3>
+					<table style="height:344px;">
+						<tr><td>Name :</td><td><input type="text" value="GTI 350 Dota Team"/></td></tr>
+						<tr><td>Informations :</td><td><textarea></textarea></td></tr>
+						<tr><td>Picture :</td><td><img class="teamImage" src="style/images/team.png" /><div style="float:right;" class="smallButton">Upload</div></tr>
+						<tr><td></td><td><div class="smallButton" style="float:right;">Save</div></td></tr>
+					</table>
+			<?php
+				include('contentBoxFooter.php'); 
+			?>
+			</div>
+		
+			<div class="columnRight">
+			<?php
+				include('contentBoxHeader.php'); 
+			?>
+				<h3>Player list</h3>
+					<table style="height:344px;">
+						<tr><td>Username</td><td></td></tr>
+						<tr><td><a href="#">Player 1</a></td><td><img src="style/images/x.gif" /></td></tr>
+						<tr><td><a href="#">Player 2</a></td><td><img src="style/images/x.gif" /></td></tr>
+						<tr><td><a href="#">Player 3</a></td><td><img src="style/images/x.gif" /></td></tr>
+						<tr><td><a href="#">Player 4</a></td><td><img src="style/images/x.gif" /></td></tr>
+						<tr><td><a href="#">Player 5</a></td><td><img src="style/images/x.gif" /></td></tr>
+					</table>
+			<?php
+				include('contentBoxFooter.php'); 
+			?>
+			</div>
+			
+			<div style="width:50%">
+			<?php
+				include('contentBoxHeader.php'); 
+			?>
+				<h3>Player requests to join the team</h3>
+				<table id="playerRequest" style="height:344px;">
 					<tr><td>Username</td><td></td></tr>
-					<tr><td><a href="#">Player 1</a></td><td><img src="style/images/x.gif" /></td></tr>
-					<tr><td><a href="#">Player 2</a></td><td><img src="style/images/x.gif" /></td></tr>
-					<tr><td><a href="#">Player 3</a></td><td><img src="style/images/x.gif" /></td></tr>
-					<tr><td><a href="#">Player 4</a></td><td><img src="style/images/x.gif" /></td></tr>
-					<tr><td><a href="#">Player 5</a></td><td><img src="style/images/x.gif" /></td></tr>
+					<tr><td><a href="#">Player 6</a></td><td><div class="smallButton">Accept</div></td><td><div class="smallButton">Decline</div></td></tr>
+					<tr><td><a href="#">Player 7</a></td><td><div class="smallButton">Accept</div></td><td><div class="smallButton">Decline</div></td></tr>
+					<tr><td><a href="#">Player 8</a></td><td><div class="smallButton">Accept</div></td><td><div class="smallButton">Decline</div></td></tr>
 				</table>
-			</td>
-		</tr>
-		<tr>
-			<td>Player requests to join the team</td>
-			<table id="playerRequest" style="border:1px solid black; width:500px;height:344px;">
-				<tr><td>Username</td><td></td></tr>
-				<tr><td><a href="#">Player 6</a></td><td><div class="button">Accept</div></td><td><div class="button">Decline</div></td></tr>
-				<tr><td><a href="#">Player 7</a></td><td><div class="button">Accept</div></td><td><div class="button">Decline</div></td></tr>
-				<tr><td><a href="#">Player 8</a></td><td><div class="button">Accept</div></td><td><div class="button">Decline</div></td></tr>
-			</table>
-		</tr>
+			<?php
+				include('contentBoxFooter.php'); 
+			?>
+			</div>
+			
+		</div>
+		<?php
+			include('mainContentBoxFooter.php'); 
+		?>
 	
 <?php
 	include('footer.php'); 
