b7d6d2bb49e1affcf6670b4e8790e05f213775cc
statick33 <alexgilbert33@hotmail.com>
2014-03-27 13:48:06 -0400

    aasdasd

--- lab3/menu.php
+++ lab3/menu.php
@@ -5,10 +5,8 @@
 		<li class="button"><a href="index.php" alt="Home"> Home</a></li>
 		<li class="button"><a href="teams.php" alt="Team Standings">Team Standings</a></li>
 		<li class="button"><a href="players.php" alt="Players"> Players</a></li>
-		<li class="button"><a href="player.php" alt="player"> Alex Gilbert</a></li>
 		<li class="button"><a href="manageTeam.php" alt="My Team"> My Team</a></li>
 		<li class="button"><a href="player.php" alt="player"> Alex Gilbert</a></li>
-		<li class="button"><a href="unauth.php" alt="Logout"> Logout</a></li>
 		<li class="button"><a href="unauth.php" alt="Logout"> Logout</a></li>
 		<li id="frameMenu2"><img src="style/images/menu2.png" /></li>
 	</ul>
