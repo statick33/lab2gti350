0f41e65d00fcb3a29f2a79b2bebdfa9afbf06948
statick33 <alexgilbert33@hotmail.com>
2014-03-27 13:43:02 -0400

    changé l'ordre du menu

--- lab3/menu.php
+++ lab3/menu.php
@@ -5,11 +5,10 @@
 		<li class="button"><a href="index.php" alt="Home"> Home</a></li>
 		<li class="button"><a href="teams.php" alt="Team Standings">Team Standings</a></li>
 		<li class="button"><a href="players.php" alt="Players"> Players</a></li>
-		<li class="button">Alex Gilbert </li>
 		<li class="button"><a href="manageTeam.php" alt="My Team"> My Team</a></li>
+		<li class="button"><a href="player.php" alt="player"> Alex Gilbert</a></li>
+		<li class="button"><a href="unauth.php" alt="Logout"> Logout</a></li>
 		<li id="frameMenu2"><img src="style/images/menu2.png" /></li>
 	</ul>
-	<div id="subMenu">
-		<a href="unauth.php">Logout</a>
-	</div>
+	
 </div>
